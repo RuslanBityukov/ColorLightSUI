7712c3d75230f5885ab1e58071b925db1f9fc420
Ruslan Bityukov <bitukov_ruslan@mail.ru>
2022-01-20 20:50:11 +0300

    Initial Commit

---
+++ ColorLightSUI/ColorLightSUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  ColorLightSUIApp.swift
+//  ColorLightSUI
+//
+//  Created by Руслан Битюков on 20.01.2022.
+//
+
+import SwiftUI
+
+@main
+struct ColorLightSUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
